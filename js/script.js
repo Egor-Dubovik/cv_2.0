04e56b1a312e241cfe94b88aa23a4ad36dde197b
Egor Dubovik <egorka54896@gmail.com>
2023-01-26 00:53:11 +0300

    feet: add preloader and change avatar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ import { switchMenu } from "./modules/switchMenu.js";
 import { scrollToblock } from "./modules/scrollToBlock.js";
 import { switchTabs } from "./modules/switchTabs.js";
 
-switchMenu();
-scrollToblock();
-switchTabs();
+const preloader = document.querySelector(".preloader");
+
+window.onload = function () {
+  setTimeout(() => {
+    if (!preloader.classList.contains("_done")) {
+      preloader.classList.add("_done");
+    }
+    switchMenu();
+    scrollToblock();
+    switchTabs();
+  }, 500);
+};
